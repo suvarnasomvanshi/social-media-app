b607bf367dae3c47d9872184254c14b1f73e0ed4
suvarna somvanshi <somavanshisuvarna48@gmail.com>
2023-07-17 19:54:44 +0530

    creating userSchema

--- backend folder/app.js
+++ backend folder/app.js
@@ -1,9 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+
+
 
 const app = express();
 
-app.use("/api",(req,res,next)=>{
-    res.send("hell world")
-})
 
-app.listen(5000)
+mongoose.connect(
+    "mongodb+srv://admin:nD8dt64B20xlG0k6@cluster0.r2aztl7.mongodb.net/?retryWrites=true&w=majority"
+)
+.then(()=>app.listen(5000))
+.then(()=>console.log("connected to database & listening to port 5000"))
+.catch((err)=>console.log(err))
